8521ca39f301c82dabb3576106c284f1b990c0bf
MykolaDekusar <ndekusar97@gmail.com>
2025-07-20 16:06:38 +0200

    Add project files.

---
+++ C#SimpleSumCalculator/Program.cs
@@ -0,0 +1,9 @@
+﻿//Chiedo il primo numero
+Console.WriteLine("Inserisci il primo numero: ");
+//Ricevo il primo numero in formato stringa e lo converto a integer
+int primoNumero = int.Parse(Console.ReadLine());
+
+//Ricevo il secondo numero in formato stringa e lo converto a integer
+int secondoNumero = int.Parse(Console.ReadLine());
+
+Console.ReadKey();
