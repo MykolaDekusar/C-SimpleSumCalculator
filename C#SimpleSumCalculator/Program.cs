a96c978230c0d9e0967fb53cc989e49154e3b1e3
MykolaDekusar <ndekusar97@gmail.com>
2025-07-20 16:34:19 +0200

    Added sum with 2 double numbers

--- C#SimpleSumCalculator/Program.cs
+++ C#SimpleSumCalculator/Program.cs
@@ -12,4 +12,20 @@ int somma = primoNumero + secondoNumero;
 
 Console.WriteLine($"La somma di {primoNumero} + {secondoNumero} è = {somma}");
 
+//Ora usiamo i numeri con la virgola
+//Chiedo il primo numero
+Console.WriteLine("Inserisci il primo numero: ");
+//Ricevo il primo numero in formato stringa e lo converto a integer
+double terzoNumero = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Inserisci il secondo numero: ");
+//Ricevo il secondo numero in formato stringa e lo converto a integer
+double quartoNumero = double.Parse(Console.ReadLine());
+
+//sommo i due numeri
+double sommaDue = terzoNumero + quartoNumero;
+
+Console.WriteLine($"La somma di {terzoNumero} + {quartoNumero} è = {sommaDue}");
+
+
 Console.ReadKey();
