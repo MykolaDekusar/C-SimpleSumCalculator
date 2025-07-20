870123b197f40a6fd96a6aa95a4e559a47596299
MykolaDekusar <ndekusar97@gmail.com>
2025-07-20 16:09:16 +0200

    Added sum between two numbers

--- C#SimpleSumCalculator/Program.cs
+++ C#SimpleSumCalculator/Program.cs
@@ -3,7 +3,13 @@ Console.WriteLine("Inserisci il primo numero: ");
 //Ricevo il primo numero in formato stringa e lo converto a integer
 int primoNumero = int.Parse(Console.ReadLine());
 
+Console.WriteLine("Inserisci il secondo numero: ");
 //Ricevo il secondo numero in formato stringa e lo converto a integer
 int secondoNumero = int.Parse(Console.ReadLine());
+
+//sommo i due numeri
+int somma = primoNumero + secondoNumero;
+
+Console.WriteLine(somma);
 
 Console.ReadKey();
